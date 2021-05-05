02f36335063a08024d705606d9c8bdf0d00ca9d3
Mijs <mijs.tenhove@hotmail.com>
2021-05-05 17:55:05 +0200

    character controler

--- zelf studie/Programeren/Shooter/Assets/Code/C_PlayerController.cs
+++ zelf studie/Programeren/Shooter/Assets/Code/C_PlayerController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class C_PlayerController : MonoBehaviour
 {
-    public float f_moveSpeed;
     public float f_mouseSensitivety = 100f;
-    float f_xrotation = 0f;
+    public float speed = 0.1f;
     float angleX;
     float angleY;
+
+    CharacterController controller;
 
     public Transform camara;
 
@@ -16,6 +17,8 @@ public class C_PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        controller = gameObject.GetComponent<CharacterController>();
+       
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -23,7 +26,7 @@ public class C_PlayerController : MonoBehaviour
     void Update()
     {
         Lookingaround();
-
+        movingplayer2();
     }
 
     public void Lookingaround() 
@@ -37,17 +40,44 @@ public class C_PlayerController : MonoBehaviour
 
         angleX = Mathf.Clamp(angleX, -90f, 90f);
 
-        transform.rotation = Quaternion.Euler(angleX, angleY, 0);
+        transform.rotation = Quaternion.Euler(-angleX, angleY, 0);
    
     }
+    //movement with character controler v1 
     public void movingplayer()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            var a = transform.TransformDirection(0, 0, 50); //voor
+            controller.SimpleMove(a * speed);
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            var b = transform.TransformDirection(0, -50, 0); // achter
+            controller.SimpleMove(b * speed);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            var c = transform.TransformDirection(50, 0, 0); //right
+            controller.SimpleMove(c * speed);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            var d = transform.TransformDirection(-50, 0, 0); //link
+            controller.SimpleMove(d * speed);
+        }
+    }
+
+    //movement with character controler v2  breaky
+    public void movingplayer2()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-    
+        controller.Move(move * speed * Time.deltaTime);
+
     }
 
 
